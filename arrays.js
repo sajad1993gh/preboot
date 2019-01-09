549299cd3c9a305361e267f5f7a96aee2ab81b01
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-09 18:27:25 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,22 @@
-var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+var chocolateBars = [ "snickers","hundred grand","kitkat","skittles"]
+var element = "sajad"
+function addElementToBeginningOfArray(chocolateBars,element){
+ var array= [element, ...chocolateBars]
+ return array
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
+  chocolateBars= [element, ...chocolateBars]
+  return chocolateBars
+}
+function addElementToEndOfArray(chocolateBars,element){
+  var array=[...chocolateBars, element]
+  return array
+}
+function distructivelyAddElementToEndOfArray(chocolateBars,element){
+  chocolateBars = [...chocolateBars, element]
+  return chocolateBars
+}
+var index = 0
+function accessElementInArray(chocolateBars,index){
+  return chocolateBars[index]
+}
